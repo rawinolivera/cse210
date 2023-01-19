b7d0d41bb4f2a56e2670c2961cbfdb80f52d7355
rawinolivera <oli18001@byui.edu>
2023-01-19 01:56:42 -0500

    preparation activity w3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Support Specialist";
+        job1._jobTitle = "ASOV";
+        job1._startYear = 2015;
+        job1._endYear = 2016;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistent";
+        job2._jobTitle = "ANKA ANDINA";
+        job2._startYear = 2016;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Rawin Olivera";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
